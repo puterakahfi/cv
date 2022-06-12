0608189f311d6cffe7a1d46fc03df5f806f6ca08
pkahfi <pkahfi@dwebsite.net>
2022-06-12 10:20:16 +0700

    update cv

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,14 @@
 <template>
   <div class="columns is-left" id="head">
     <div class="column is-6">
-      <div class="titleunderline is-medium has-margin-top-100"></div>
-      <h1 class="title is-size-large has-margin-bottom-5 has-text-grey-darker">{{ data.name }}</h1>
-      <h4 class="subtitle is-4 has-margin-top-10 has-text-grey">{{ data.title}}</h4>
+      <h1
+        class="title is-size-large has-margin-bottom-5 has-text-grey-darker has-margin-top-100"
+      >
+        {{ data.name }}
+      </h1>
+      <h4 class="subtitle is-4 has-margin-top-10 has-text-grey">
+        {{ data.title }}
+      </h4>
       <div class="cta">
         <a
           v-for="link in data.links"
@@ -12,33 +17,25 @@
           class="button is-white has-margin-right-5"
           target="_blank"
         >
-          <fa v-if="link.icon" :icon="[link.icon[0], link.icon[1]]"/>
+          <fa v-if="link.icon" :icon="[link.icon[0], link.icon[1]]" />
           &nbsp; {{ link.label }}
         </a>
       </div>
     </div>
-
-    <div class="column is-6">
-      <img :src="selectedImage" class="img img-fluid">
-    </div>
   </div>
 </template>
-
-
-
 
 <script>
 export default {
   props: ["data"],
   data() {
     return {
-      selectedImage: ""
+      selectedImage: "",
     };
   },
-  mounted: function() {
-    this.selectedImage = this.data.images[
-      Math.floor(Math.random() * this.data.images.length)
-    ];
-  }
+  mounted: function () {
+    this.selectedImage =
+      this.data.images[Math.floor(Math.random() * this.data.images.length)];
+  },
 };
 </script>
